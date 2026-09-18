2d4b360d65162d6ce90d5813bd998d2c51384529
hungntharry <hungnt.harry@gmail.com>
2026-09-18 15:48:39 +0700

    Add run_app.bat

---
+++ run_app.bat
@@ -0,0 +1,6 @@
+@echo off
+chcp 65001 >nul
+set PYTHONIOENCODING=utf-8
+set PATH=%PATH%;C:\Users\hungnt8\AppData\Local\Programs\Python\Python312
+python "D:\File storage\AI\AI Agent\Điện\app.py"
+pause
